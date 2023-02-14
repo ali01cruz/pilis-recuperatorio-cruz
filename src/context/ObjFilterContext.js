583ec8ad9362eba03cf2a4f0cbbb196c0e8be79d
ali <ali01cruz@gmail.com>
2023-02-14 19:50:15 -0300

    contexts

--- src/context/ObjFilterContext.js
+++ src/context/ObjFilterContext.js
@@ -8,6 +8,5 @@ export const ObjFilterContext = createContext({
 export const ObjFilterProvider = ({ children }) => {
   const [objFilter, setObjFilter] = useState([]);
   const value = { objFilter, setObjFilter };
-
   return <ObjFilterContext.Provider value={value}>{children}</ObjFilterContext.Provider>;
 }
